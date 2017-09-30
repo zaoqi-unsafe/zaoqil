e00eff7e50a81e0682a1cc9ff2b380d5a8593ea3
zaoqi <zaomir@outlook.com>
2017-09-30 22:14:03 +0800

    curry

--- core.rkt
+++ core.rkt
@@ -84,20 +84,30 @@
 (define envset hash-set)
 (define envget hash-ref)
 
+(define (capply f xs) (aapply genv f (map (λ (x) (list 'quote x)) xs)))
+
+; Prim → Func
+(define (pack n p) (%pack n (λ (xs) (capply p xs))))
+(define (%pack n g)
+  (if (zero? n)
+      (g '())
+      (func
+       (λ (x) (%pack (pred n) (λ (xs) (g (cons x xs))))))))
+
 ; Nat → (Env → [Exp] → Any) → Prim
 (struct prim (n f))
 (define (prim-apply f env xs g)
   (unlazy*+ (prim-n f) xs (λ (xs rs) (g rs ((prim-f f) env xs)))))
 (define (primm n f) (prim n (λ (env xs) (apply f (cons env xs)))))
-(define (primf n f) (prim n (λ (env xs) (apply f (map (λ (x) (eeval env x)) xs)))))
+(define (primf n f) (pack n (prim n (λ (env xs) (apply f (map (λ (x) (eeval env x)) xs))))))
 (define (primp n f)
-  (prim
-   n
-   (λ (env xs)
-     (unlazy...
-      (map (λ (x) (eeval env x)) xs)
-      (λ (as)
-        (apply f as))))))
+  (pack n (prim
+           n
+           (λ (env xs)
+             (unlazy...
+              (map (λ (x) (eeval env x)) xs)
+              (λ (as)
+                (apply f as)))))))
 
 ; (Any → Any) → Func
 (struct func (v))
@@ -114,7 +124,7 @@
 
 (define (from-f x)
   (λ as
-    (to-racket-value (aapply genv x (map (λ (a) (list 'quote (from-racket-value a))) as)))))
+    (to-racket-value (capply x (map (λ (a) (from-racket-value a)) as)))))
 
 (define (from-racket-value x)
   (cond
@@ -135,6 +145,7 @@
       [else x])))
 
 (define (f? x) (or (func? x) (macro? x) (prim? x)))
+(define (f-uncurried? x) #f)
 
 ; Env → Exp → Any
 (define (eeval env x) (delay (%eval env x)))
@@ -160,13 +171,15 @@
                (unlazy
                 ((func-v f) (eeval env (car xs)))
                 (λ (r)
-                  (if (f? r)
-                      (aapply env r (cdr xs))
-                      (unlazy
-                       (cdr xs)
-                       (λ (d)
-                         (if (null? d)
-                             r
+                  (unlazy
+                   (cdr xs)
+                   (λ (d)
+                     (if (null? d)
+                         (if (f-uncurried? r)
+                             (aapply env r '())
+                             r)
+                         (if (f? r)
+                             (aapply env r (cdr xs))
                              (_!_)))))))
                (_!_))]
           [(prim? f)
@@ -191,87 +204,88 @@
 ; Exp → Env → Env
 (define (cload e env) (env+record env (force+ (eeval env e))))
 
+(define (mkpair xs f)
+  (if (null? xs)
+      (f '())
+      (unlazy
+       (car xs)
+       (λ (s)
+         (mkpair (cddr xs)
+                 (λ (d)
+                   (f (cons (cons s (cadr xs)) d))))))))
+
 (define genv
   (cload (file->sexp "prelude.core")
-  (env
-   'quote (primm
-           1
-           (λ (env x)
-             x))
-   'λ (primm
-       2
-       (λ (env s v)
-         (unlazy
-          s
-          (λ (s)
-            (if (symbol? s)
-                (func (λ (x) (eeval (envset env s x) v)))
-                (_!_))))))
-   'cons (primf 2 cons)
-   'car (primp 1 car)
-   'cdr (primp 1 cdr)
-   'null? (primp 1 null?)
-   'true true
-   'false false
-   'boolean? (primp 1 boolean?)
-   'if (primf 3 (λ (c t f)
-                  (unlazy c (λ (c) (if c t f)))))
-   'pair? (primp 1 pair?)
-   'symbol? (primp 1 symbol?)
-   '+ (primp 2 +)
-   '- (primp 2 -)
-   '* (primp 2 *)
-   '/ (primp 2 /)
-   '> (primp 2 >)
-   '< (primp 2 <)
-   '>= (primp 2 >=)
-   '=< (primp 2 <=)
-   '= (primp 2 =)
-   'record (primm
-            0
-            (λ (env . xs)
-              (define (mkpair xs f)
-                (if (null? xs)
-                    (f '())
-                    (unlazy
-                     (car xs)
-                     (λ (s)
-                       (mkpair (cddr xs)
-                               (λ (d)
-                                 (f (cons (cons s (cadr xs)) d))))))))
-              (define newenv
-                (delay
-                  (env-append env (force+ rc))))
-              (define rc
-                (delay
-                  (mkpair
-                   xs
-                   (λ (ps)
-                     (map (λ (p)
-                            (if (symbol? (car p))
-                                (cons (car p) (delay (eeval (force newenv) (cdr p))))
-                                (_!_))) ps)))))
-              (unlazy
-               rc
-               (λ (rc)
-                 (record (make-immutable-hasheq rc))))))
-   'open (primm
-          2
-          (λ (env r x)
-            (unlazy
-             (eeval env r)
-             (λ (r)
-               (eeval (env+record env r) x)))))
-   ': (primm
-       2
-       (λ (env r x)
-         (unlazy
-          (eeval env r)
-          (λ (r)
-            (unlazy
-             x
-             (λ (s)
-               (hash-ref (record-v r) s)))))))
-   )))
+         (env
+          'quote (primm
+                  1
+                  (λ (env x)
+                    x))
+          'λ (primm
+              2
+              (λ (env s v)
+                (unlazy
+                 s
+                 (λ (s)
+                   (if (symbol? s)
+                       (func (λ (x) (eeval (envset env s x) v)))
+                       (_!_))))))
+          'cons (primf 2 cons)
+          'car (primp 1 car)
+          'cdr (primp 1 cdr)
+          'null? (primp 1 null?)
+          'true true
+          'false false
+          'boolean? (primp 1 boolean?)
+          'if (primf 3 (λ (c t f)
+                         (unlazy c (λ (c) (if c t f)))))
+          'pair? (primp 1 pair?)
+          'symbol? (primp 1 symbol?)
+          '+ (primp 2 +)
+          '- (primp 2 -)
+          '* (primp 2 *)
+          '/ (primp 2 /)
+          '> (primp 2 >)
+          '< (primp 2 <)
+          '>= (primp 2 >=)
+          '=< (primp 2 <=)
+          '= (primp 2 =)
+          'record (primm
+                   0
+                   (λ (env . xs)
+                     (define newenv
+                       (delay
+                         (env-append env (force+ rc))))
+                     (define rc
+                       (delay
+                         (mkpair
+                          xs
+                          (λ (ps)
+                            (map (λ (p)
+                                   (if (symbol? (car p))
+                                       (cons (car p) (delay (eeval (force newenv) (cdr p))))
+                                       (_!_))) ps)))))
+                     (unlazy
+                      rc
+                      (λ (rc)
+                        (record (make-immutable-hasheq rc))))))
+          'open (primm
+                 2
+                 (λ (env r x)
+                   (unlazy
+                    (eeval env r)
+                    (λ (r)
+                      (eeval (env+record env r) x)))))
+          ': (primm
+              2
+              (λ (env r x)
+                (unlazy
+                 (eeval env r)
+                 (λ (r)
+                   (unlazy
+                    x
+                    (λ (s)
+                      (hash-ref (record-v r) s)))))))
+          )))
 
 (define (ceval x) (to-racket-value (eeval genv x)))
