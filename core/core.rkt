5dff3dd7d2bd48a7204c00b0315f796543f6e173
zaoqi <zaomir@outlook.com>
2017-10-29 17:24:41 +0800

    core.rkt

---
+++ core/core.rkt
@@ -0,0 +1,35 @@
+#lang racket
+;;  Copyright (C) 2017  Zaoqi
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Affero General Public License as published
+;;  by the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Affero General Public License for more details.
+
+;;  You should have received a copy of the GNU Affero General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(provide core)
+(define (memorize1eq f)
+  (let ([m (make-weak-hasheq)])
+    (λ (x) (hash-ref m x
+                     (λ ()
+                       (let ([v (f x)])
+                         (hash-set! m x v)
+                         v))))))
+(define (memroizeeq f)
+  (let ([t (memorize1eq (λ (xs) (apply f xs)))])
+    (λ xs (t xs))))
+(define-syntax-rule (delay-force x) (delay (force x)))
+(define (with-exception-handler handler thunk)
+  (with-handlers ([(λ (x) #t) handler]) (thunk)))
+(module s racket
+  (provide (rename-out [structt struct]))
+  (define-syntax-rule (structt x ...)
+    (struct x ... #:transparent)))
+(require 's)
+(include "core.scm")
