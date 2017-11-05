06adda210aee5e5c85c66102c0a800ad7f9550b3
zaoqi <zaomir@outlook.com>
2017-11-05 18:19:21 +0800

    开始重写

---
+++ core.rkt
@@ -0,0 +1,139 @@
+#lang racket
+;;  Copyright (C) 2017  Zaoqi
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Affero General Public License as published
+;;  by the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Affero General Public License for more details.
+
+;;  You should have received a copy of the GNU Affero General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define (memorize1eq f)
+  (let ([m (make-weak-hasheq)])
+    (λ (x) (hash-ref m x
+                     (λ ()
+                       (let ([v (f x)])
+                         (hash-set! m x v)
+                         v))))))
+(define (memroizeeq f)
+  (let ([t (memorize1eq (λ (xs) (apply f xs)))])
+    (λ xs (t xs))))
+(define-syntax-rule (delay-force x) (delay (force x)))
+(define (with-exception-handler handler thunk)
+  (with-handlers ([(λ (x) #t) handler]) (thunk)))
+(module s racket
+  (provide (rename-out [structt struct]))
+  (define-syntax-rule (structt x ...)
+    (struct x ... #:transparent)))
+(require 's)
+
+
+(define (succ x) (+ 1 x))
+(define (pred x) (- x 1))
+(define (force+ x)
+  (if (promise? x)
+      (force+ (force x))
+      x))
+(define (unlazy x f)
+  (if (promise? x)
+      (if (promise-forced? x)
+          (unlazy (force x) f)
+          (delay (unlazy (force x) f)))
+      (f x)))
+(define (lmap f xs)
+  (unlazy
+   xs
+   (λ (xs)
+     (if (null? xs)
+         '()
+         (cons (f (car xs)) (lmap f (cdr xs)))))))
+(define (unlazy* xs f)
+  (if (null? xs)
+      (f '())
+      (unlazy
+       (car xs)
+       (λ (x)
+         (unlazy*
+          (cdr xs)
+          (λ (d)
+            (f (cons x d))))))))
+(define (unlazylist xs f)
+  (unlazy
+   xs
+   (λ (xs)
+     (if (null? xs)
+         (f '())
+         (unlazylist
+          (cdr xs)
+          (λ (d)
+            (f (cons (car xs) d))))))))
+
+; U 'undefined 'syntax -> Symbol -> [Env * Exp] -> CompileErr
+(struct compile-error (t f xs))
+(define undefined 'undefined)
+(define syntaxerr 'syntax)
+(define (err t f xs) (raise (compile-error t f xs)))
+
+; Env -> String -> Symbol -> [Any] -> String -> TypeError
+(struct type-error (env at f parm i))
+
+; String → Nat → [U Symbol (Promise Hash) Hash] → At
+(struct at (file line ss))
+(define (at+ x s) (at (at-file x) (at-line x) (cons s (at-ss x))))
+
+(struct env (at x))
+(define (hash->env h) (env (at "" 0 (list h)) h))
+(define (newenv . xs) (hash->env (apply hasheq xs)))
+(define (env-set e s x) (env (env-at e) (hash-set (env-x e) s x)))
+(define (env-get e s f) (hash-ref (env-x e) s f))
+(define (env-at+ e x) (env (at+ (env-at e) x) (env-x e)))
+
+; f : Any -> Any
+(struct lam1 (exp f))
+; f : Stream Any -> Any
+(struct lam... (exp f))
+; n : Nat
+; f : Env -> Exp -> ... -> Any
+(struct f-n (exp n f))
+; f : Env -> Stream Exp -> Any
+(struct f... (exp f))
+
+(define genv
+  (newenv
+   'true #t
+   'false #f
+   ))
+
+(define (EVAL env x)
+  (delay
+    (unlazy
+     x
+     (λ (x)
+       (cond
+         [(pair? x)
+          (unlazy
+           (car x)
+           (λ (xa)
+             (cond
+               [(eq? xa 'G)
+                (unlazy
+                 (cdr x)
+                 (λ (xd)
+                   (EVAL genv (car xd))))]
+               [(eq? xa '!)
+                (unlazy
+                 (cdr x)
+                 (λ (xd)
+                   (APPLYmacro env (car xd) (cdr xd))))]
+               [else (APPLY env (EVAL env xa) (lmap (λ (x) (EVAL env x)) (cdr x)))])))]
+         [(symbol? x)
+          (env-get env x
+                   (λ () (raise (compile-error undefined 'eval (list (cons env x))))))]
+         [(string? x) (string->list x)]
+         [else x])))))
