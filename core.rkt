a440f2390bba68e20b407f1f68f4a39f5b248480
zaoqi <zaomir@outlook.com>
2017-11-10 20:05:57 +0800

    暂时未使用

--- core.rkt
+++ core.rkt
@@ -242,3 +242,161 @@
       [(and (pair? x) (list? x) (andmap char? x)) (list->string x)]
       [else x])))
 (define (core x) (torkt (EVAL genv x)))
+
+
+; ----------------------------------------------------------------------------------------------------------------------------
+; 暂时未使用
+
+(define (choice2 x y f)
+  (if (promise? x)
+      (delay (choice2 y (force x) f))
+      (f x y)))
+;wip
+;(define (%choice* ps xs rs)
+;  (if (pair? ps)
+;      (%choice* (cdr ps) (cons (car ps) xs) rs)
+;      (if (null? ps)
+;          (%choice*- xs rs)
+;          (if (null? xs)
+;              (if (null? rs)
+;                  (unlazy ps (λ (ps) (%choice* ps '() '())))
+;                  (%choice* ps rs '()))
+;              (if (promise? (car xs))
+;                  (delay
+;                    (delay
+;                      (%choice* (force ps) (cdr xs) (cons (force (car xs)) rs))))
+;                  (cons (car xs) (%choice* ps (cdr xs) rs)))))))
+
+(struct reads (x r))
+(define (readst s) (readc (string->list s)))
+(define (readc xs)
+  (or (read-comment xs)
+      (read-boolean xs)
+      (read-list xs)
+      (read-char xs)
+      (read-quote xs)
+      (read-space xs)
+      (read-number xs)
+      (read-symbol xs)))
+(define (read-comment xs)
+  (and (eq? (car xs) #\;)
+       (%read-comment (cdr xs))))
+(define (%read-comment xs)
+  (if (eq? (car xs) #\newline)
+      (readc (cdr xs))
+      (%read-comment (cdr xs))))
+(define (read-boolean xs)
+  (and (eq? (car xs) #\#)
+       (let ([nxs (cdr xs)])
+         (or (and (eq? (car nxs) #\t) (reads #t (cdr nxs)))
+             (and (eq? (car nxs) #\f) (reads #f (cdr nxs)))))))
+(define (read-list xs)
+  (and (eq? (car xs) #\()
+       (%read-list (cdr xs))))
+(define (%read-list xs)
+  (if (eq? (car xs) #\))
+      (reads '() (cdr xs))
+      (let ([x (readc xs)])
+        (and x (let ([rs (%read-list (reads-r x))])
+                 (and rs
+                      (reads (cons (reads-x x) (reads-x rs)) (reads-r rs))))))))
+(define (read-char xs)
+  (and (eq? (car xs) #\#)
+       (let ([nxs (cdr xs)])
+         (and (eq? (car xs) #\\)
+              (let ([nnxs (cdr nxs)])
+                (reads (car nnxs) (cdr nnxs)))))))
+(define (read-quote xs)
+  (and (eq? (car xs) #\')
+       (let ([x (readc (cdr xs))])
+         (and x (reads (list 'quote (reads-x x)) (reads-r x))))))
+(define (space? x)
+  (or (eq? x #\space) (eq? x #\tab) (eq? x #\return) (eq? x #\newline)))
+(define (delimiters? x)
+  (or (space? x) (eq? x #\() (eq? x #\)) (eq? x #\[) (eq? x #\]) (eq? x #\") (eq? x #\,) (eq? x #\') (eq? x #\`) (eq? x #\;)))
+(define (read-space xs)
+  (let ([x (car xs)])
+    (and (space? x)
+         (readc (cdr xs)))))
+(define (read-symbol xs)
+  (let ([x (%read-symbol xs)])
+    (and x
+         (reads (string->symbol (list->string (reads-x x))) (reads-r x)))))
+(define (%read-symbol xs)
+  (and
+   (not (null? xs))
+   (not (delimiters? (car xs)))
+   (let ([n (%read-symbol (cdr xs))])
+     (if n
+         (reads (cons (car xs) (reads-x n)) (reads-r n))
+         (reads (list (car xs)) (cdr xs))))))
+(define (n? x)
+  (or (eq? x #\0) (eq? x #\1) (eq? x #\2) (eq? x #\3) (eq? x #\4) (eq? x #\5) (eq? x #\6) (eq? x #\7) (eq? x #\8) (eq? x #\9)))
+(define (read-number xs)
+  (let ([x (%read-number xs)])
+    (and x
+         (reads (string->number (list->string (reads-x x))) (reads-r x)))))
+(define (%read-number xs)
+  (if (eq? (car xs) #\.)
+      (let ([x (%%read-number (cdr xs))])
+        (and x
+             (reads (cons (car xs) (reads-x x)) (reads-r x))))
+      (and (n? (car xs))
+           (let ([n (%read-number (cdr xs))])
+             (if n
+                 (reads (cons (car xs) (reads-x n)) (reads-r n))
+                 (reads (list (car xs)) (cdr xs)))))))
+(define (%%read-number xs)
+  (and (n? (car xs))
+       (let ([n (%%read-number (cdr xs))])
+         (if n
+             (reads (cons (car xs) (reads-x n)) (reads-r n))
+             (reads (list (car xs)) (cdr xs))))))
+
+
+(struct ioret (v))
+(struct iocall/ccv (id x))
+(struct iocall/cc (id x))
+(struct iobind (x f))
+(struct ioputstr (x))
+(struct ionewline ())
+(struct ioread-line ())
+(define (io? x) (or (ioret? x) (iocall/ccv? x) (iocall/cc? x) (iobind? x) (ioputstr? x) (ionewline? x) (ioread-line? x)))
+(define id!
+  (let ([idc 0])
+    (λ ()
+      (set! idc (succ idc))
+      idc)))
+; old
+;(define (runio x f)
+;  (unlazy
+;   x
+;   (λ (x)
+;     (cond
+;       [(ioret? x) (f (ioret-v x))]
+;       [(iobind? x)
+;        (runio (iobind-x x) (λ (r) (unlazy
+;                                    (iobind-f x)
+;                                    (λ (x)
+;                                      (runio (capply x (list r)) f)))))]
+;       [(ioputstr? x) (unlazy* (ioputstr-x x) (λ (s) (display (list->string s)) (f '())))]
+;       [(ionewline? x) (newline) (f '())]
+;       [(iocall/ccv? x) x]
+;       [(iocall/cc? x) (runio (iocall/cc-x x)
+;                              (λ (r)
+;                                (if (and (iocall/ccv? r) (equal? (iocall/ccv-id r) (iocall/cc-id x)))
+;                                    (f (iocall/ccv-x r))
+;                                    (f r))))]
+;       [(ioread-line? x) (f (read-line))]
+;       [else (err syntaxerr 'runio (list x f) '())]))))
+;(define io
+;  (record (hasheq 'return (pf 1 ioret)
+;                  '>>= (pf 2 iobind)
+;                  'putstr (pf 1 ioputstr)
+;                  'newline (ionewline)
+;                  'readline (ioread-line)
+;                  'call/cc (pf
+;                            1
+;                            (λ (f)
+;                              (let ([id (id!)])
+;                                (iocall/cc id (capply f (list (pf 1 (λ (x) (iocall/ccv id x))))))))))))
