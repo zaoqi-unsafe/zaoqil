edf6f36a3ffee3c8742f8b68fcc551e0475e7b46
zaoqi <zaomir@outlook.com>
2017-11-24 20:06:22 +0800

    ->

--- core/core.rkt
+++ core/core.rkt
@@ -21,6 +21,10 @@
   (define-syntax-rule (structt x ...)
     (struct x ... #:transparent)))
 (require 's)
+(define (zsplit-at xs n) (let-values ([(h t) (split-at xs n)]) (cons h t)))
+
+(define (succ x) (+ 1 x))
+(define (pred x) (- x 1))
 
 (struct undefined (t id x))
 (struct UnDeF ())
@@ -65,3 +69,74 @@
                 (cdr d)
                 (λ (dd) (u (cons a (cons (car d) dd))))
                 (λ (ps) (f (cons (cons a (car d)) ps))))))))))))
+
+(define newenv hasheq)
+(define env-set hash-set)
+(define env-get hash-ref)
+(define-syntax-rule (_!_) (error "_|_")) ; BUG
+
+; e = 表达式, f : [Any] -> Any, n : Nat
+(struct λn (e n f))
+; f : [Any] -> [Any] -> Any, n : PosInt
+(struct λn... (e n f))
+; f : Env -> [Exp] -> Any, n : Nat
+(struct Fn (e n f))
+; f : Env -> [Exp] -> [Exp] -> Any, n : PosInt
+(struct Fn... (e n f))
+
+(define (EVAL env x)
+  (unlazy
+   x
+   (λ (x) (list '! '(! _G chenv) env x))
+   (λ (x)
+     (cond
+      [(pair? x)
+       (unlazy2list
+        x
+        (λ (xs) (list '! '(! _G chenv) env xs))
+        (λ (xs)
+          (unlazy
+           (car xs)
+           (λ (a) (list '! '(! _G chenv) env (cons a (cdr xs))))
+           (λ (a)
+             (cond
+               [(eq? a '!) (APPLYmacro env (EVAL env (cadr xs)) (cddr xs))]
+               [else (APPLY (EVAL env a) (map (λ (x) (EVAL env x)) (cdr xs)))])))))]
+      [(symbol? x) (if (eq? x '_G)
+                       (env-get genv '_G)
+                       (env-get env x))]
+      [(string? x) (string->list x)]
+      [else x]))))
+(define (APPLYmacro env f xs)
+  (unlazy
+   f
+   (λ (f) (list '! '(! _G chenv) env (cons '! (cons f xs))))
+   (λ (f)
+     (cond
+       [(Fn? f) (let ([sr (zsplit-at xs (Fn-n f))])
+                  (if (null? (cdr sr))
+                  ((Fn-f f) env (car sr))
+                  (_!_)))]
+       [(Fn...? f) (let ([sr (zsplit-at xs (Fn...-n f))])
+                     ((Fn...-f f) env (car sr) (cdr sr)))]
+       [else (_!_)]))))
+(define (QUOTE x) (list '! '(! _G quote) x))
+(define (APPLY f xs)
+  (unlazy
+   f
+   (λ (f) (list '! '(! _G chenv) env (cons f (map QUOTE xs))))
+   (λ (f)
+     (cond
+       [(λn? f) (let ([sr (zsplit-at xs (λn-n f))])
+                  (if (null? (cdr sr))
+                  ((λn-f f) (car sr))
+                  (_!_)))]
+       [(λn...? f) (let ([sr (zsplit-at xs (λn...-n f))])
+                     ((λn...-f f) (car sr) (cdr sr)))]
+       [else (_!_)]))))
+(define genv
+  (newenv
+   'true #t
+   'false #f
+   ))
+  
