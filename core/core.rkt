bf8507aa36683504e7ad654c4da4e93c9e56ce64
zaoqi <zaomir@outlook.com>
2017-12-02 11:40:10 +0800

    read

--- core/core.rkt
+++ core/core.rkt
@@ -13,130 +13,107 @@
 
 ;;  You should have received a copy of the GNU Affero General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-(define-syntax-rule (delay-force x) (delay (force x)))
-(define (with-exception-handler handler thunk)
-  (with-handlers ([(λ (x) #t) handler]) (thunk)))
-(module s racket
-  (provide (rename-out [structt struct]))
-  (define-syntax-rule (structt x ...)
-    (struct x ... #:transparent)))
-(require 's)
-(define (zsplit-at xs n) (let-values ([(h t) (split-at xs n)]) (cons h t)))
+(provide mayberead mustread)
+(struct reads (x r))
+(define (mayberead s)
+  (let ([x (readc (string->list s))])
+    (and
+     x
+     (null? (reads-r x))
+     (list (reads-x x)))))
+(define (mustread s) (car (mayberead s)))
+(define (readc xs)
+  (or (read-comment xs)
+      (read-boolean xs)
+      (read-macro xs)
+      (read-list xs)
+      (read-char xs)
+      (read-quote xs)
+      (read-space xs)
+      (read-number xs)
+      (read-symbol xs)))
+(define (read-comment xs)
+  (and (eq? (car xs) #\;)
+       (%read-comment (cdr xs))))
+(define (%read-comment xs)
+  (if (eq? (car xs) #\newline)
+      (readc (cdr xs))
+      (%read-comment (cdr xs))))
+(define (read-boolean xs)
+  (and (eq? (car xs) #\#)
+       (let ([nxs (cdr xs)])
+         (or (and (eq? (car nxs) #\t) (reads #t (cdr nxs)))
+             (and (eq? (car nxs) #\f) (reads #f (cdr nxs)))))))
+(define (mk-read-xs b e f)
+  (define (readxs xs)
+    (and (eq? (car xs) b)
+         (let ([x (%readxs (cdr xs))])
+           (and x (reads (f (reads-x x)) (reads-r x))))))
+  (define (%readxs xs)
+    (if (eq? (car xs) e)
+        (reads '() (cdr xs))
+        (let ([x (readc xs)])
+          (and x (let ([rs (skip-space (reads-r x) %readxs)])
+                   (and rs
+                        (reads (cons (reads-x x) (reads-x rs)) (reads-r rs))))))))
+  readxs)
+(define read-list (mk-read-xs #\( #\) (λ (xs) xs)))
+(define read-macro (mk-read-xs #\{ #\} (λ (xs) (cons '! xs))))
+(define (read-char xs)
+  (and (eq? (car xs) #\#)
+       (let ([nxs (cdr xs)])
+         (and (eq? (car nxs) #\\)
+              (let ([nnxs (cdr nxs)])
+                (reads (car nnxs) (cdr nnxs)))))))
+(define (read-quote xs)
+  (and (eq? (car xs) #\')
+       (let ([x (readc (cdr xs))])
+         (and x (reads (list '! 'quote (reads-x x)) (reads-r x))))))
+(define (space? x)
+  (or (eq? x #\space) (eq? x #\tab) (eq? x #\return) (eq? x #\newline)))
+(define (delimiters? x)
+  (or (space? x) (eq? x #\#) (eq? x #\() (eq? x #\)) (eq? x #\{) (eq? x #\}) (eq? x #\") (eq? x #\,) (eq? x #\') (eq? x #\`) (eq? x #\;)))
+(define (read-space xs)
+  (let ([x (car xs)])
+    (and (space? x)
+         (readc (cdr xs)))))
+(define (skip-space xs f)
+  (let ([x (car xs)])
+    (if (space? x)
+        (skip-space (cdr xs) f)
+        (f xs))))
+(define (read-symbol xs)
+  (let ([x (%read-symbol xs)])
+    (and x
+         (reads (string->symbol (list->string (reads-x x))) (reads-r x)))))
+(define (%read-symbol xs)
+  (and
+   (not (null? xs))
+   (not (delimiters? (car xs)))
+   (let ([n (%read-symbol (cdr xs))])
+     (if n
+         (reads (cons (car xs) (reads-x n)) (reads-r n))
+         (reads (list (car xs)) (cdr xs))))))
+(define (n? x)
+  (or (eq? x #\0) (eq? x #\1) (eq? x #\2) (eq? x #\3) (eq? x #\4) (eq? x #\5) (eq? x #\6) (eq? x #\7) (eq? x #\8) (eq? x #\9)))
+(define (read-number xs)
+  (let ([x (%read-number xs)])
+    (and x
+         (reads (string->number (list->string (reads-x x))) (reads-r x)))))
+(define (%read-number xs)
+  (if (eq? (car xs) #\.)
+      (let ([x (%%read-number (cdr xs))])
+        (and x
+             (reads (cons (car xs) (reads-x x)) (reads-r x))))
+      (and (n? (car xs))
+           (let ([n (%read-number (cdr xs))])
+             (if n
+                 (reads (cons (car xs) (reads-x n)) (reads-r n))
+                 (reads (list (car xs)) (cdr xs)))))))
+(define (%%read-number xs)
+  (and (n? (car xs))
+       (let ([n (%%read-number (cdr xs))])
+         (if n
+             (reads (cons (car xs) (reads-x n)) (reads-r n))
+             (reads (list (car xs)) (cdr xs))))))
 
-(define (succ x) (+ 1 x))
-(define (pred x) (- x 1))
-
-(struct undefined (t id x))
-(struct UnDeF ())
-(define UNDEF (UnDeF))
-(define UNDEF? UnDeF?)
-
-(define (unlazy u x f)
-  (cond
-   [(promise? x)
-    (if (promise-forced? x)
-        (unlazy u (force x) f)
-        (delay (unlazy u (force x) f)))]
-   [(undefined? x) (undefined (undefined-t x) (undefined-id x) (u (undefined-x x)))]
-   [else (f x)]))
-(define (unlazy2list xs u f)
-  (unlazy
-   xs
-   u
-   (λ (xs)
-     (if (null? xs)
-         (f '())
-         (unlazy2list
-          (cdr xs)
-          (λ (d) (u (cons (car xs) d)))
-          (λ (d) (f (cons (car xs) d))))))))
-(define (unlazy2pairs xs u f) ; u接近[Any] -> Any, f : [Pairof NotPromise Any] -> Any
-  (unlazy
-   xs
-   u
-   (λ (xs)
-     (if (null? xs)
-         (f '())
-         (unlazy
-          (car xs)
-          (λ (a) (u (cons a (cdr xs))))
-          (λ (a)
-            (unlazy
-             (cdr xs)
-             (λ (d) (u (cons a d)))
-             (λ (d)
-               (unlazy2pairs
-                (cdr d)
-                (λ (dd) (u (cons a (cons (car d) dd))))
-                (λ (ps) (f (cons (cons a (car d)) ps))))))))))))
-
-(define newenv hasheq)
-(define env-set hash-set)
-(define env-get hash-ref)
-(define-syntax-rule (_!_) (error "_|_")) ; BUG
-
-; e = 表达式, f : [Any] -> Any, n : Nat
-(struct λn (e n f))
-; f : [Any] -> [Any] -> Any, n : PosInt
-(struct λn... (e n f))
-; f : Env -> [Exp] -> Any, n : Nat
-(struct Fn (e n f))
-; f : Env -> [Exp] -> [Exp] -> Any, n : PosInt
-(struct Fn... (e n f))
-
-(define (EVAL env x)
-  (unlazy
-   x
-   (λ (x) (list '! '(! _G chenv) env x))
-   (λ (x)
-     (cond
-      [(pair? x)
-       (unlazy2list
-        x
-        (λ (xs) (list '! '(! _G chenv) env xs))
-        (λ (xs)
-          (unlazy
-           (car xs)
-           (λ (a) (list '! '(! _G chenv) env (cons a (cdr xs))))
-           (λ (a)
-             (cond
-               [(eq? a '!) (APPLYmacro env (EVAL env (cadr xs)) (cddr xs))]
-               [else (APPLY (EVAL env a) (map (λ (x) (EVAL env x)) (cdr xs)))])))))]
-      [(symbol? x) (if (eq? x '_G)
-                       (env-get genv '_G)
-                       (env-get env x))]
-      [(string? x) (string->list x)]
-      [else x]))))
-(define (APPLYmacro env f xs)
-  (unlazy
-   f
-   (λ (f) (list '! '(! _G chenv) env (cons '! (cons f xs))))
-   (λ (f)
-     (cond
-       [(Fn? f) (let ([sr (zsplit-at xs (Fn-n f))])
-                  (if (null? (cdr sr))
-                  ((Fn-f f) env (car sr))
-                  (_!_)))]
-       [(Fn...? f) (let ([sr (zsplit-at xs (Fn...-n f))])
-                     ((Fn...-f f) env (car sr) (cdr sr)))]
-       [else (_!_)]))))
-(define (QUOTE x) (list '! '(! _G quote) x))
-(define (APPLY f xs)
-  (unlazy
-   f
-   (λ (f) (cons f (map QUOTE xs)))
-   (λ (f)
-     (cond
-       [(λn? f) (let ([sr (zsplit-at xs (λn-n f))])
-                  (if (null? (cdr sr))
-                  ((λn-f f) (car sr))
-                  (_!_)))]
-       [(λn...? f) (let ([sr (zsplit-at xs (λn...-n f))])
-                     ((λn...-f f) (car sr) (cdr sr)))]
-       [else (_!_)]))))
-(define genv
-  (newenv
-   'true #t
-   'false #f
-   ))
-  
---
+++ core/test.rkt
@@ -0,0 +1,22 @@
+#lang racket
+;;  Copyright (C) 2017  Zaoqi
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Affero General Public License as published
+;;  by the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Affero General Public License for more details.
+
+;;  You should have received a copy of the GNU Affero General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(require "core.rkt")
+(require rackunit)
+
+(check-equal? (mustread "{record x 0}") '(! record x 0))
+(check-equal? (mustread "'a") '(! quote a))
+(check-equal? (mustread "( 1 2 3        4      )") '(1 2 3 4))
+(check-equal? (mustread "#\\a") #\a)
